df0a5f3b1acf880a11172647ed294aff077aeee7
timerminator <timerbaevruslan@gmail.com>
2020-08-04 13:18:00 +0500

    Add files via upload
    Change create_commendation

--- scripts.py
+++ scripts.py
@@ -24,30 +24,30 @@ def remove_chastisements(child):
 
 
 def create_commendation(child, subject):
+    commendation_text = random.choice(['Молодец!', 'Отлично!', 'Гораздо лучше, чем я ожидал!', 'Хорошо!',
+                                       'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!',
+                                       'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+                                       'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!',
+                                       'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!',
+                                       'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!',
+                                       'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!',
+                                       'Это как раз то, что нужно!', 'Я тобой горжусь!',
+                                       'С каждым разом у тебя получается всё лучше!',
+                                       'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!',
+                                       'Ты растешь над собой!', 'Ты многое сделал, я это вижу!',
+                                       'Теперь у тебя точно все получится!'])
     try:
-        lessons = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=subject)
-        lesson = lessons.order_by('-date').first()
         schoolkid = Schoolkid.objects.get(full_name__contains=child)
-        commendation_text = random.choice(['Молодец!', 'Отлично!', 'Гораздо лучше, чем я ожидал!', 'Хорошо!',
-                                           'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!',
-                                           'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
-                                           'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!',
-                                           'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!',
-                                           'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!',
-                                           'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!',
-                                           'Это как раз то, что нужно!', 'Я тобой горжусь!',
-                                           'С каждым разом у тебя получается всё лучше!',
-                                           'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!',
-                                           'Ты растешь над собой!', 'Ты многое сделал, я это вижу!',
-                                           'Теперь у тебя точно все получится!'])
-        Commendation.objects.create(text=commendation_text, created=lesson.date, schoolkid=schoolkid,
-                                    subject=lesson.subject, teacher=lesson.teacher)
+        lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter,
+                                        subject__title=subject)
+        lesson = lessons.order_by('-date').first()
+        if lesson is None:
+            print('Такой предмет отсутствует.')
+        else:
+            Commendation.objects.create(text=commendation_text, created=lesson.date, schoolkid=schoolkid,
+                                        subject=lesson.subject, teacher=lesson.teacher)
     except ObjectDoesNotExist:
         print("Школьник с таким именем отсутствует.")
     except MultipleObjectsReturned:
         print("С таким именем есть несколько школьников.")
 
-
-fix_marks('Фролов Иван')
-remove_chastisements('Фролов Иван')
-create_commendation('Фролов Иван', 'Музыка')
